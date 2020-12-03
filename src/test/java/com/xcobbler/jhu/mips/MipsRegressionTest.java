fb38d39644478dfc42e845a662044b346630c3d7
Xavier <xavier@sample.com>
2020-12-03 01:08:24 -0500

    adds support for template tests to test multiple registers

--- src/test/java/com/xcobbler/jhu/mips/MipsRegressionTest.java
+++ src/test/java/com/xcobbler/jhu/mips/MipsRegressionTest.java
@@ -6,7 +6,10 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import org.junit.Assert;
@@ -33,6 +36,7 @@ public class MipsRegressionTest {
     this.checks = checks;
   }
 
+  @SuppressWarnings("unchecked")
   @Parameterized.Parameters(name = "{index} - {0}")
   public static Collection<Object[]> testCases() throws IOException {
     Collection<Object[]> ret = new ArrayList<Object[]>();
@@ -45,32 +49,83 @@ public class MipsRegressionTest {
       File asmFile = new File("src/test/resources/regression/" + f);
 
       String asm = new String(Files.readAllBytes(asmFile.toPath()));
-      List<Check> checks = new ArrayList<Check>();
       if(asm.contains(".assert")) {
-        String assertSection = asm.substring(asm.indexOf(".assert") + 7).trim();
-        List<String> lines = Arrays.asList(assertSection.split("\n")).stream().filter(s -> !s.trim().isEmpty())
-            .collect(Collectors.toList());
-        for (String line : lines) {
-          if (line.trim().isEmpty() || line.trim().startsWith("#")) {
-            continue;
-          }
-          String[] parts = line.split("=");
-          if (parts.length != 2) {
-            throw new IllegalArgumentException("assert doesn't have exactly one equals sign: " + line);
-          }
-          String reg = parts[0].trim();
-          reg = reg.replace("$", "");
-          String val = parts[1].trim();
+        
+//        asm = asm.substring(0, asm.indexOf(".assert"));
 
-          RegCheck check = new RegCheck(reg, Integer.valueOf(val));
-          checks.add(check);
+        List<String> allLines = Arrays.asList(asm.split("\n")).stream().filter(s -> !s.trim().isEmpty())
+            .collect(Collectors.toList());
+
+        List<Object[]> tests = handleSplitting(allLines);
+
+        for (Object[] test : tests) {
+          asm = ((List<String>) test[1]).stream().collect(Collectors.joining("\n"));
+
+          String assertSection = asm.substring(asm.indexOf(".assert") + 7).trim();
+          List<String> assertLines = Arrays.asList(assertSection.split("\n")).stream().filter(s -> !s.trim().isEmpty())
+              .collect(Collectors.toList());
+          List<Check> checks = new ArrayList<Check>();
+          for (String line : assertLines) {
+            if (line.trim().isEmpty() || line.trim().startsWith("#")) {
+              continue;
+            }
+            String[] parts = line.split("=");
+            if (parts.length != 2) {
+              throw new IllegalArgumentException("assert doesn't have exactly one equals sign: " + line);
+            }
+            String reg = parts[0].trim();
+            reg = reg.replace("$", "");
+            String val = parts[1].trim();
+
+            RegCheck check = new RegCheck(reg, Integer.valueOf(val));
+            checks.add(check);
+          }
+          
+          ret.add(new Object[] { f.replace(".txt", "") + test[0], asm.substring(0, asm.indexOf(".assert")), checks });
         }
-        asm = asm.substring(0, asm.indexOf(".assert"));
+
       } else {
         throw new IllegalArgumentException(".assert section must exist at the end!");
       }
-      
-      ret.add(new Object[] { f.replace(".txt", ""), asm, checks });
+    }
+
+    return ret;
+  }
+
+  // 0 - String name extension
+  // 1 - List<String> lines
+  private static List<Object[]> handleSplitting(List<String> lines) {
+    List<Object[]> ret = new ArrayList<Object[]>();
+    Map<String, List<String>> replacements = new HashMap<String, List<String>>();
+
+    for (String line : lines) {
+      line = line.trim();
+      if (line.startsWith("$")) {
+        String[] parts = line.split("=");
+        String valsPart = parts[1].substring(1, parts[1].length() - 1);
+        String[] vals = valsPart.split("\\|");
+        replacements.put(parts[0].trim(), Arrays.asList(vals));
+      } else {
+        break;
+      }
+    }
+
+    lines = lines.subList(replacements.keySet().size(), lines.size());
+
+    if (replacements.keySet().size() == 0) {
+      ret.add(new Object[] { "", lines });
+    } else {
+      List<String> toReplace = lines.subList(replacements.keySet().size(), lines.size());
+      // TODO only supports single line replacements
+      for (Entry<String, List<String>> entry : replacements.entrySet()) {
+        for (String val : entry.getValue()) {
+          List<String> subList = new ArrayList<String>();
+          for (String s : toReplace) {
+            subList.add(s.replace(entry.getKey(), val));
+          }
+          ret.add(new Object[] { "-" + val.replace("$", ""), subList });
+        }
+      }
     }
 
     return ret;
